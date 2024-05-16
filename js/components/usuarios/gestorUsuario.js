9e7b7a3a7831b05e7ee240864d2bcfd2b14255db
unknown <anacollazoslo@uch.pe>
2024-05-15 19:54:36 -0500

    mi 1er commit

---
+++ js/components/usuarios/gestorUsuario.js
@@ -0,0 +1,12 @@
+class GestorUsuarios{
+    constructor(){
+        //todas las variables que deben inicializarse
+        this.usuarios = []; // Los usuario de la funcion GestorUsuarios, es un array
+        this.init();
+    }
+    init(){
+        console.log("Holi")
+    }
+}
+
+export default GestorUsuarios
---
+++ js/myapp.js
@@ -0,0 +1,6 @@
+import GestorUsuarios from '../js/components/usuarios/gestorUsuario.js';
+
+$(document).ready(function(){
+
+    const gestorUsuario = new GestorUsuarios();
+})
